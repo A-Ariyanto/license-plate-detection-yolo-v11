ad5285b9ea54b6bd05a056c16ba94c7e7978c618
Abdullah Ariyanto <z5543164@ad.unsw.edu.au>
2025-07-02 08:47:41 +1000

    Fixed test.py

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import os
 from ultralytics import YOLO
 
-# === Testing Trained Yolo Model ===
+
 def test_model(model):
-    results = model("demo.mp4", save=True)
+    print("Testing YOLOv11 model on a demo")
+    
+    results = model("image1.jpg", save=True)
+    
+    print("Results saved")
+    
+    return results
+    
+    
+if __name__ == "__main__":
+    model = YOLO("runs/detect/train/weights/best.pt")
+    
+    result = test_model(model)
+    
+    print("Testing completed. Check the output in the 'runs/detect/predict' directory.")
